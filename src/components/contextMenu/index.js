28fd11d9730adc06ccc242d5af8cf2dcc3333e24
OseungKwon <00osos@naver.com>
2022-04-15 02:53:51 +0900

    BUILD : fontawsome install

---
+++ src/components/contextMenu/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const index = () => {
+  return (
+    <div>
+      <div>
+        <FontAwesomeIcon icon="fa-solid fa-trash-can" />
+      </div>
+    </div>
+  );
+};
+
+export default index;
