2c941786293f6fb6b1c460d78e4de775ddfdd677
OseungKwon <00osos@naver.com>
2022-04-28 00:04:35 +0900

    TEST : testcode에 redux 세팅 추가

--- src/components/__test__/Header.test.js
+++ src/components/__test__/Header.test.js
@@ -1,15 +1,33 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import Header from "../header/";
+import * as reactRedux from "react-redux";
+
+jest.mock("react-redux", () => ({
+  useSelector: jest.fn(),
+  useDispatch: jest.fn()
+}));
 
 describe("<Header/>", () => {
+  const useSelectorMock = jest.spyOn(reactRedux, "useSelector");
+  const useDispatchMock = jest.spyOn(reactRedux, "useDispatch");
+
+  beforeEach(() => {
+    useSelectorMock.mockClear();
+    useDispatchMock.mockClear();
+  });
+
   it("pocket text 확인", () => {
     render(<Header />);
     screen.getByText(/Pocket/i);
   });
   it("pocket icon 확인", () => {
     render(<Header />);
-    const iconElement = screen.getByTestId("titleIcon");
-    expect(iconElement).toBeVisible();
+    const titleIcon = screen.getByTestId("titleIcon");
+    expect(titleIcon).toBeVisible();
+  });
+  it("setting icon 확인", () => {
+    render(<Header />);
+    const titleIcon = screen.getByTestId("titleIcon");
+    expect(titleIcon).toBeVisible();
   });
 });
